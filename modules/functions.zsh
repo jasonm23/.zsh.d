f584d862d9963d8e11e8538af7172968c777ac3d
Jason Milkins <jasonm23@gmail.com>
2022-05-11 14:23:07 +0700

    Add google_translate_line
    Usage:
    
    google_translate_line $source_language $target_language "$text_to_translate"
    
    example:
    
    google_translate_line en th "Some example foo bar text"

--- modules/functions.zsh
+++ modules/functions.zsh
@@ -363,3 +363,18 @@ randomize_timestamps_in_folder () {
     echo "$a $tstamp"
   done
 }
+
+google_translate_line () {
+  source_lang="$1"
+  target_lang="$2"
+  text="$3"
+
+  local escaped="sl=$source_lang&tl=$target_lang&q=$text"
+
+  echo $(wget -qO- \
+    -U "AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1" \
+    --header "'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'" \
+    --header "'User-Agent': 'AndroidTranslate/5.3.0.RC02.130475354-53000263 5.1 phone TRANSLATE_OPM5_TEST_1'" \
+    --post-data "$escaped" \
+    "https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=$target_lang&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e")
+}
