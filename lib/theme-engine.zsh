ef1ce3308c0ce8647889fc2d8f80aaa76c30983e
jasonm23 <jasonm23@gmail.com>
2022-05-25 23:02:44 +0700

    Slim down prompt add thorough git-info

--- lib/theme-engine.zsh
+++ lib/theme-engine.zsh
@@ -13,10 +13,76 @@ ZSH_THEME_GIT_PROMPT_SUFFIX="$fg_bold[red])"
 ZSH_THEME_GIT_PROMPT_CLEAN=""
 ZSH_THEME_GIT_PROMPT_DIRTY=" ✨ "
 ZSH_THEME_IS_SSH_SYMBOL="⚡️️ "
+ZSH_THEME_GIT_PROMPT_BEHIND=""
+
+setopt prompt_subst
+autoload -U colors && colors # Enable colors in prompt
+
+# Echoes a username/host string when connected over SSH (empty otherwise)
+ssh_info() {
+  [[ "$SSH_CONNECTION" != '' ]] && echo '%(!.%{$fg[red]%}.%{$fg[yellow]%})%n%{$reset_color%}@%{$fg[green]%}%m%{$reset_color%}:' || echo ''
+}
+
+# Echoes information about Git repository status when inside a Git repository
+git_info() {
+
+  # Exit if not inside a Git repository
+  ! git rev-parse --is-inside-work-tree > /dev/null 2>&1 && return
+
+  # Git branch/tag, or name-rev if on detached head
+  local GIT_LOCATION=${$(git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD)#(refs/heads/|tags/)}
+
+  local AHEAD="%{$fg[red]%}⇡NUM%{$reset_color%}"
+  local BEHIND="%{$fg[cyan]%}⇣NUM%{$reset_color%}"
+  local MERGING="%{$fg[magenta]%}⚡︎%{$reset_color%}"
+  local UNTRACKED="%{$fg[red]%}●%{$reset_color%}"
+  local MODIFIED="%{$fg[yellow]%}●%{$reset_color%}"
+  local STAGED="%{$fg[green]%}●%{$reset_color%}"
+
+  local -a DIVERGENCES
+  local -a FLAGS
+
+  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+  if [ "$NUM_AHEAD" -gt 0 ]; then
+    DIVERGENCES+=( "${AHEAD//NUM/$NUM_AHEAD}" )
+  fi
+
+  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+  if [ "$NUM_BEHIND" -gt 0 ]; then
+    DIVERGENCES+=( "${BEHIND//NUM/$NUM_BEHIND}" )
+  fi
+
+  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+    FLAGS+=( "$MERGING" )
+  fi
+
+  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+    FLAGS+=( "$UNTRACKED" )
+  fi
+
+  if ! git diff --quiet 2> /dev/null; then
+    FLAGS+=( "$MODIFIED" )
+  fi
+
+  if ! git diff --cached --quiet 2> /dev/null; then
+    FLAGS+=( "$STAGED" )
+  fi
+
+  local -a GIT_INFO
+  GIT_INFO+=( "\033[38;5;15m±" )
+  [ -n "$GIT_STATUS" ] && GIT_INFO+=( "$GIT_STATUS" )
+  [[ ${#DIVERGENCES[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)DIVERGENCES}" )
+  [[ ${#FLAGS[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)FLAGS}" )
+  GIT_INFO+=( "\033[38;5;15m$GIT_LOCATION%{$reset_color%}" )
+  echo "$FG[$Bc][${(j: :)GIT_INFO}$FG[$Bc]]"
+
+}
+
 
 # optional line-break
 obreak="
 "
 # The final % (or #) prompt is always on a new line.
-PROMPT='$FG[$Bc][%# $FG[$Nc]%n$FG[$Bc]@$(is_ssh)$FG[$Mc]%M$FG[$Bc]|$FG[$Tc]%D{%I:%M%p} $FG[$Dc]%D{%a %d/%b} (%D{%z})$FG[$Bc]]$FG[$Bc][$(git_prompt_info)$FG[$RVMc]$(rvm_prompt_info)$FG[$Bc]][$FG[$Pc]$(git_pair_info)$FG[$Bc]]$obreak$FG[$Bc][$FG[$Pc]%~$FG[$Bc]]$reset_color
+PROMPT='$FG[$Bc][%# $FG[$Nc]%n$FG[$Bc]@$(is_ssh)$FG[$Mc]%M$FG[$Bc]|$FG[$Tc]%D{%I:%M%p}$FG[$Bc]]$(git_info)$obreak$FG[$Bc][$FG[$Pc]%~$FG[$Bc]]$reset_color
 '
