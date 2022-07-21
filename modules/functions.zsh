946b379016df45ce3de7cb74987d5d392e74646b
jasonm23 <jasonm23@gmail.com>
2022-07-21 23:07:48 +0700

    get-missing-subtitles

--- modules/functions.zsh
+++ modules/functions.zsh
@@ -467,3 +467,32 @@ get_thai () {
 	url="https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=th&dt=t&q=${encoded}" 
 	curl -s "${url}" | perl -pe 's/^.*?"(.*?)".*$/\1/'
 }
+
+get-missing-subtitles () {
+	folder="$(movie-missing-srt | fzf)" 
+	if [[ -d "${folder}" ]]
+	then
+		movie=$(fd "(mp4|mkv|avi)$" "${folder}" | fzf) 
+		if [[ -f "${movie}" ]]
+		then
+			get_subs "${movie}"
+			downloaded="${movie%.*}.srt" 
+			if [[ -f "${downloaded}" ]]
+			then
+				english_srt="${movie%.*}.en.srt" 
+				mv "${movie%.*}.srt" "${english_srt}"
+				echo "${folder}:"
+				ls -G -s1 "$folder"
+				open -a Progression
+				srt translate "${english_srt}" > "${english_srt/.en./.th.}"
+			else
+				echo "Sorry, no subtitles were downloaded for $(basename "${movie}")"
+				return 1
+			fi
+		else
+			return 1
+		fi
+	else
+		return 1
+	fi
+}
