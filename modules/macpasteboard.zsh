468dd0d0d3550ae25e443d6960704ec9dc6e0f10
Jason Milkins <jasonm23@gmail.com>
2022-07-27 20:10:45 +0700

    reorganize macpasteboard

--- modules/macpasteboard.zsh
+++ modules/macpasteboard.zsh
@@ -1,47 +1,45 @@
- pb-kill-line () {
+pb-kill-line () {
   zle kill-line
   echo -n $CUTBUFFER | pbcopy
 }
+zle -N pb-kill-line
 
 pb-kill-whole-line () {
   zle kill-whole-line
   echo -n $CUTBUFFER | pbcopy
 }
+bindkey "\ek"  pb-kill-whole-line
+zle -N pb-kill-whole-line
 
 pb-backward-kill-word () {
   zle backward-kill-word
   echo -n $CUTBUFFER | pbcopy
 }
+zle -N pb-kill-word
 
 pb-kill-word () {
   zle kill-word
   echo -n $CUTBUFFER | pbcopy
 }
+zle -N pb-kill-word
 
 pb-kill-buffer () {
   zle kill-buffer
   echo -n $CUTBUFFER | pbcopy
 }
+bindkey "\e^b" pb-kill-buffer
+zle -N pb-kill-buffer
 
 pb-copy-region-as-kill-deactivate-mark () {
   zle copy-region-as-kill
   zle set-mark-command -n -1
   echo -n $CUTBUFFER | pbcopy
 }
+zle -N pb-copy-region-as-kill-deactivate-mark
+bindkey "\eK"  pb-copy-region-as-kill-deactivate-mark
 
 pb-yank () {
   CUTBUFFER=$(pbpaste)
   zle yank
 }
-
-zle -N pb-kill-line
-zle -N pb-kill-whole-line
-zle -N pb-backward-kill-word
-zle -N pb-kill-word
-zle -N pb-kill-buffer
-zle -N pb-copy-region-as-kill-deactivate-mark
 zle -N pb-yank
-
-bindkey "\ek"  pb-kill-whole-line
-bindkey "\eK"  pb-copy-region-as-kill-deactivate-mark
-bindkey "\e^b" pb-kill-buffer
