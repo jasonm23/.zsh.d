2e4ba6db935f83559db74230a963e2c7cd9cf76e
jasonm23 <jasonm23@gmail.com>
2022-07-31 14:40:10 +0700

    Use HEREDOC instead of separate lines/array for messages.

--- modules/printbox.zsh
+++ modules/printbox.zsh
@@ -10,12 +10,24 @@ longest_str_len() {
   echo $MAX
 }
 
+printboxsq0() {
+  LEN=$( longest_str_len $@)
+  ((MAX=LEN + 2))
+  printf "┌"; printf "%0.s─" {1..$MAX}; printf "┐\n"
+  for line in $@; do
+    printf "│ ${line}"; printf "%0.s " {1..$(( MAX - ${#line} - 1 ))}; printf "│\n"
+  done
+  printf "└"; printf "%0.s─" {1..$MAX}; printf "┘\n"
+}
+
 printboxsq() {
   LEN=$( longest_str_len $@)
   ((MAX=LEN + 2))
-
   printf "┌"; printf "%0.s─" {1..$MAX}; printf "┐\n"
   for line in $@; do
+    if [[ "$line" == "$@[$#]" ]];then
+      printf "├"; printf "%0.s─" {1..$MAX}; printf "┤\n"
+    fi
     printf "│ ${line}"; printf "%0.s " {1..$(( MAX - ${#line} - 1 ))}; printf "│\n"
   done
   printf "└"; printf "%0.s─" {1..$MAX}; printf "┘\n"
--- modules/ssh-helpers.zsh
+++ modules/ssh-helpers.zsh
@@ -1,24 +1,51 @@
 # Macos specific check for multiple ssh-agents and running the launchd ssh-agent.
 
+ssh-fix-env() {
+  SSH_AGENT_COUNT=$(pgrep ssh-agent | wc -l | tr -d " \n")
+  if (( $SSH_AGENT_COUNT == 0 )); then
+    ssh-agent -l # Mac specific.
+  fi
+
+  export SSH_AUTH_SOCK=$(launchctl getenv SSH_AUTH_SOCK)
+  export SSH_AGENT_PID=$(pgrep ssh-agent | head -1)
+
+	FIX=$(cat <<-FIX
+	SSH_AUTH_SOCK=$(launchctl getenv SSH_AUTH_SOCK)
+	SSH_AGENT_PID=$(pgrep ssh-agent | head -1)
+	Environment set/exported/active.
+	FIX
+  )
+  SSH_FIX_MESSAGE=("${(@f)FIX}")
+
+  if [[ "$1" != "quiet" ]]; then
+    printbox $SSH_FIX_MESSAGE
+  fi
+}
+
+
 ssh-agent-check() {
   SSH_AGENT_COUNT=$(pgrep ssh-agent | wc -l | tr -d " \n")
   SSH_AGENT_PLURAL=$((( SSH_AGENT_COUNT == 1 )) && echo "" || echo "s")
   DETECTED_PID=$(pgrep ssh-agent | head -1)
 
-  LINES_OK=(
-     "($SSH_AGENT_COUNT) ssh-agent${SSH_AGENT_PLURAL} running on ${DETECTED_PID}."
-     "SSH_AUTH_SOCK: $SSH_AUTH_SOCK"
-     "SSH_AGENT_PID: $SSH_AGENT_PID"
-     "SSH env ok."
+	OK=$(cat <<-OK
+	($SSH_AGENT_COUNT) ssh-agent${SSH_AGENT_PLURAL} running on ${DETECTED_PID}.
+	SSH_AUTH_SOCK: $SSH_AUTH_SOCK
+	SSH_AGENT_PID: $SSH_AGENT_PID
+	SSH env ok.
+	OK
   )
+  LINES_OK=("${(@f)OK}")
 
-  LINES_WARN=(
-     "($SSH_AGENT_COUNT) ssh-agent${SSH_AGENT_PLURAL} running on ${DETECTED_PID}."
-     "SSH_AUTH_SOCK: $SSH_AUTH_SOCK"
-     "SSH_AGENT_PID: $SSH_AGENT_PID"
-     "Launchd ssh-agent (sock): $(launchctl getenv SSH_AUTH_SOCK)"
-     "SSH env not set correctly, run ssh-fix-env\n"
+  WARN=$(cat <<-WARN
+	($SSH_AGENT_COUNT) ssh-agent${SSH_AGENT_PLURAL} running on ${DETECTED_PID}.
+	SSH_AUTH_SOCK: $SSH_AUTH_SOCK
+	SSH_AGENT_PID: $SSH_AGENT_PID
+	Launchd ssh-agent (sock): $(launchctl getenv SSH_AUTH_SOCK)
+	SSH env not set correctly, run ssh-fix-env
+	WARN
   )
+  LINES_WARN=("${(@f)WARN}")
 
   if ((${SSH_AGENT_COUNT} != 1)); then
     echo "(${SSH_AGENT_COUNT}) ssh-agents are running"
@@ -31,26 +58,7 @@ ssh-agent-check() {
   fi
 }
 
-ssh-fix-env() {
-  SSH_AGENT_COUNT=$(pgrep ssh-agent | wc -l | tr -d " \n")
-  if (( $SSH_AGENT_COUNT == 0 )); then
-    ssh-agent -l # Mac specific.
-  fi
-
-  export SSH_AUTH_SOCK=$(launchctl getenv SSH_AUTH_SOCK)
-  export SSH_AGENT_PID=$(pgrep ssh-agent | head -1)
-
-  SSH_FIX_MESSAGE=(
-   "SSH_AUTH_SOCK=$(launchctl getenv SSH_AUTH_SOCK)"
-   "SSH_AGENT_PID=$(pgrep ssh-agent | head -1)"
-   "Environment set/exported/active."
-  )
-
-  if [[ "$1" != "quiet" ]]; then
-    printbox $SSH_FIX_MESSAGE
-  fi
-}
-
 is_ssh() {
-  [[ -n "$SSH_CLIENT" ]] && echo $ZSH_THEME_IS_SSH_SYMBOL
+  [[ -n $SSH_CLIENT ]] && echo $ZSH_THEME_IS_SSH_SYMBOL
 }
+
