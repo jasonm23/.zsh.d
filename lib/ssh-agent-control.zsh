bba897e616c862a7be220a836ebce45d7983adfe
jasonm23 <jasonm23@gmail.com>
2025-05-20 17:26:41 +0700

    Improved ssh-agent reconnect

--- lib/ssh-agent-control.zsh
+++ lib/ssh-agent-control.zsh
@@ -1,20 +1,25 @@
 # source from .zshrc
 
+PIDOF_AGENT_SOCK="$(ps -ax | rg "$(pidof ssh-agent).*?-a.*?\s*(/.*)" -r '$1')"
+export PIDOF_AGENT_SOCK="${PIDOF_AGENT_SOCK#"${PIDOF_AGENT_SOCK%%[![:space:]]*}"}"
+
 if [[ $USER == root ]]; then
     echo "Root user, skipping ssh-agent setup."
 else
-  if [[ $SSH_AUTH_SOCK == "$(ps -ax | rg "$(pidof ssh-agent).*?-a.*?(/.*)" -r '$1')" ]]; then
-      echo "Connecting to Gnome Keyring ssh-agent"
+  if [[ $SSH_AUTH_SOCK =~ "/run/user/$UID/keyring/" ]]; then
+      echo "Connecting to Gnome keyring ssh-agent"
+      echo "${PIDOF_AGENT_SOCK}"
   elif [[ $SSH_AUTH_SOCK == $HOME/.ssh/sock ]]; then
       echo "Connecting to zsh.d ssh-agent"
-      export SSH_AGENT_PID=$(pidof ssh-agent)
   elif [[ -n $SSH_AUTH_SOCK ]]; then
-       echo "ssh-agent on ${SSH_AUTH_SOCK}"
+      echo "ssh-agent on ${SSH_AUTH_SOCK}"
+      echo "$PIDOF_AGENT_SOCK"
   elif [[ -z $SSH_AUTH_SOCK ]]; then
       if pidof ssh-agent; then
           echo "ssh-agent found at $(pidof ssh-agent)"
+          echo "${PIDOF_AGENT_SOCK}"
       else
-          echo "no ssh-agent found, starting from .zsh.d"
+          echo "ssh-agent not found, starting from .zsh.d"
           eval $(ssh-agent -a $HOME/.ssh/sock)
       fi
   fi
