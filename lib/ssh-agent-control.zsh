c32f931ecf6704f55ed631667cce38a289fd1f9f
jasonm23 <jasonm23@gmail.com>
2025-05-20 21:15:19 +0700

    Testing ssh-agent resolution on helios/linux-mint

--- lib/ssh-agent-control.zsh
+++ lib/ssh-agent-control.zsh
@@ -1,17 +1,26 @@
 # source from .zshrc
 
-PIDOF_AGENT_SOCK="$(ps -ax | rg "$(pidof ssh-agent).*?-a.*?\s*(/.*)" -r '$1')"
-export PIDOF_AGENT_SOCK="${PIDOF_AGENT_SOCK#"${PIDOF_AGENT_SOCK%%[![:space:]]*}"}"
-
 if [[ $USER == root ]]; then
     echo "Root user, skipping ssh-agent setup."
 else
-  if [[ $SSH_AUTH_SOCK =~ "${XDG_RUNTIME_DIR}/keyring/" ]]; then
-      echo "Connecting to Gnome keyring ssh-agent"
-  elif [[ $SSH_AUTH_SOCK == $HOME/.ssh/sock ]]; then
-      echo "Connecting to zsh.d ssh-agent"
-  elif [[ $SSH_AUTH_SOCK =~ "${XDG_RUNTIME_DIR}/wezterm/agent" ]]; then
-      echo "WezTerm ssh-agent proxy Gnome keyring"
-  fi
-  ssh-add -l 2>&1 | awk '{print "key: ",$3, $1, $4}'
+    if [[ -S /run/user/$UID/keyring/ssh ]]; then
+        if [[ $SSH_AUTH_SOCK =~ "${XDG_RUNTIME_DIR}/keyring/" ]]; then
+            echo "Connected to Gnome keyring ssh-agent"
+        fi
+    else
+        PIDOF_AGENT_SOCK="$(ps -ax | rg "$(pidof ssh-agent).*?-a.*?\s*(/.*)" -r '$1')"
+        export PIDOF_AGENT_SOCK="${PIDOF_AGENT_SOCK#"${PIDOF_AGENT_SOCK%%[![:space:]]*}"}"
+        if [[ -s "$PIDOF_AGENT_SOCK" ]]; then
+            export SSH_AUTH_SOCK="$PIDOF_AGENT_SOCK"
+        fi
+
+        if [[ $SSH_AUTH_SOCK == $HOME/.ssh/sock ]]; then
+            echo "Connecting to zsh.d ssh-agent"
+        fi
+
+        if [[ $SSH_AUTH_SOCK =~ "${XDG_RUNTIME_DIR}/wezterm/agent" ]]; then
+            echo "WezTerm ssh-agent proxy Gnome keyring"
+        fi
+    fi
+    ssh-add -l 2>&1 | awk '{print "key: ",$3, $1, $4}'
 fi
