3a8710f20f1a4afb4b9a339429dee5a4891c9bdd
jasonm23 <jasonm23@gmail.com>
2025-05-20 21:28:16 +0700

    Better ssh-agent connection for Ubuntu and Mint

--- lib/ssh-agent-control.zsh
+++ lib/ssh-agent-control.zsh
@@ -4,31 +4,33 @@ if [[ $USER == root ]]; then
     echo "Root user, skipping ssh-agent setup."
 else
     if [[ -S /run/user/$UID/keyring/ssh ]]; then
-        if [[ $SSH_AUTH_SOCK =~ "/run/user/$UID/keyring/" ]]; then
-            echo "Connected to Gnome keyring ssh-agent"
-        else
+        if [[ ! $SSH_AUTH_SOCK =~ "/run/user/$UID/keyring/" ]]; then
             export SSH_AUTH_SOCK=/run/user/$UID/keyring/ssh
         fi
     elif [[ -S /run/user/$UID/keyring/.ssh ]]; then
-        if [[ $SSH_AUTH_SOCK =~ "/run/user/$UID/keyring/" ]]; then
-            echo "Connected to Gnome keyring ssh-agent"
-        else
+        if [[ ! $SSH_AUTH_SOCK =~ "/run/user/$UID/keyring/" ]]; then
             export SSH_AUTH_SOCK=/run/user/$UID/keyring/.ssh
         fi
     else
         PIDOF_AGENT_SOCK="$(ps -ax | rg "$(pidof ssh-agent).*?-a.*?\s*(/.*)" -r '$1')"
         export PIDOF_AGENT_SOCK="${PIDOF_AGENT_SOCK#"${PIDOF_AGENT_SOCK%%[![:space:]]*}"}"
-        if [[ -s "$PIDOF_AGENT_SOCK" ]]; then
+        if [[ -S "$PIDOF_AGENT_SOCK" ]]; then
             export SSH_AUTH_SOCK="$PIDOF_AGENT_SOCK"
         fi
-
-        if [[ $SSH_AUTH_SOCK == $HOME/.ssh/sock ]]; then
-            echo "Connecting to zsh.d ssh-agent"
-        fi
-
-        if [[ $SSH_AUTH_SOCK =~ "${XDG_RUNTIME_DIR}/wezterm/agent" ]]; then
-            echo "WezTerm ssh-agent proxy Gnome keyring"
-        fi
     fi
+
+    # Checks on SSH Sock
+    if [[ $SSH_AUTH_SOCK == $HOME/.ssh/sock ]]; then
+        echo "Connecting to zsh.d ssh-agent"
+    fi
+
+    if [[ $SSH_AUTH_SOCK =~ "${XDG_RUNTIME_DIR}/keyring" ]]; then
+        echo "Connected to Gnome keyring ssh-agent"
+    fi
+
+    if [[ $SSH_AUTH_SOCK =~ "${XDG_RUNTIME_DIR}/wezterm/agent" ]]; then
+        echo "WezTerm ssh-agent proxy Gnome keyring"
+    fi
+
     ssh-add -l 2>&1 | awk '{print "key: ",$3, $1, $4}'
 fi
