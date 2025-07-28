0d5b468c366fa4172119e658ec4c68a2fbd55a0e
jasonm23 <jasonm23@gmail.com>
2025-07-28 15:25:58 +0700

    Updated vite react builder

--- modules/vite.zsh
+++ modules/vite.zsh
@@ -7,10 +7,163 @@ patch_tsconfig() {
 }
 
 patch_viteconfig() {
-  sed -E -i -e 's|plugins: \[(.*react.*)\]|plugins: [\1, tailwindcss()]|' vite.config.ts 2>/dev/null
-  cat <(<<<"import tailwindcss from '@tailwindcss/vite'") vite.config.ts | tee vite_temp
-  mv vite_temp vite.config.ts
-  bat -P --style plain vite.config.ts
+cat <<EOF > vite.config.ts
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  server: {
+    allowedHosts: []
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src/'),
+    },
+  },
+  plugins: [react(), tailwindcss()],
+})
+EOF
+}
+
+patch_app_tsx() {
+    cat <<EOF > src/App.tsx
+import { Heading } from '@/components/heading';
+import { ThemeProvider } from '@/contexts/theme-provider';
+import { cn } from '@/lib/utils';
+import type { FC } from 'react';
+
+export const App:FC = () => (
+  <ThemeProvider>
+    <div>
+      <Heading titl='$1'/>
+    </div>
+  </ThemeProvider>
+)
+EOF
+}
+
+patch_use_local_storage() {
+    mkdir -p src/hooks
+    cat <<EOF > src/hooks/use-local-storage.ts
+import { useState } from 'react';
+
+export const useLocalStorage = (key: string, initialValue: any) => {
+  const [value, setValue] = useState(() => {
+    try {
+      const storedValue = localStorage.getItem(key);
+      if (storedValue != undefined) {
+        return JSON.parse(storedValue)
+      } else {
+        localStorage.setItem(key, JSON.stringify(initialValue));
+        return initialValue
+      }
+
+    } catch (error) {
+      console.error(error);
+      localStorage.setItem(key, JSON.stringify(initialValue));
+      return initialValue;
+    }
+  });
+
+  const saveToLocalStorage = (newValue: any) => {
+    setValue(newValue);
+    localStorage.setItem(key, JSON.stringify(newValue));
+  };
+
+  return [value, saveToLocalStorage];
+};
+EOF
+}
+
+patch_theme_provider() {
+    mkdir -p src/contexts
+    cat <<EOF > src/contexts/theme-provider.tsx
+import React, { useEffect } from "react";
+import type { FC, ReactNode } from "react";
+import { useLocalStorage } from "@/hooks/use-local-storage";
+import { ThemeContext } from "@/contexts/theme-context";
+
+export const ThemeProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const [theme, setTheme] = useLocalStorage("theme", "light");
+
+  useEffect(() =>
+    theme == 'dark'
+      ? document.body.classList.add('dark')
+      : document.body.classList.remove('dark')
+  , [theme])
+
+  const toggleTheme = () =>
+    (theme == 'light')
+      ? setTheme('dark')
+      : setTheme('light')
+
+
+  return (
+    <ThemeContext.Provider value={{
+      theme,
+      toggleTheme
+    }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+EOF
+}
+
+patch_theme_context() {
+    mkdir -p src/contexts
+    cat <<EOF > src/contexts/theme-context.tsx
+import { createContext } from "react";
+
+export const ThemeContext = createContext({
+  theme: 'light',
+  toggleTheme: () => {}
+});
+
+EOF
+}
+
+patch_heading() {
+    cat <<EOF > src/components/heading.tsx
+import { Menu, Moon, Sun } from 'lucide-react'
+import { toast } from 'sonner'
+import { useContext, useEffect } from 'react'
+import { ThemeContext } from '@/contexts/theme-context'
+
+interface HeadingProps {
+  title: string
+}
+
+function Heading(props: HeadingProps) {
+  const { title } = props
+  const { toggleTheme, theme } = useContext(ThemeContext)
+
+  useEffect(() => (
+    console.log(`Theme: ${theme}`)
+  ), [theme])
+
+  return (
+    <header className="flex items-center justify-between p-4 border-b">
+      <div className="p-2 hover:bg-accent cursor-pointer rounded-lg">
+        <Menu className="h-6 w-6" />
+      </div>
+      <div className="font-black tracking-tighter text-2xl">{title}</div>
+      <div
+        className="p-2 hover:bg-accent cursor-pointer rounded-lg"
+        onClick={() => toggleTheme()}
+      >
+        {theme === 'light' ? <Moon className="h-6 w-6" /> : <Sun className="h-6 w-6" />}
+      </div>
+    </header>
+  )
+}
+
+export { Heading }
+
+EOF
 }
 
 patch_css() {
@@ -27,31 +180,45 @@ vite_tailwind_ts_react_shadcn (){
     pnpm create vite@latest $1 -- --template react-ts
     cd $1
     pnpm install
-    echo "\n🔨 patching vite.config.ts...\n"
+    echo "\n🔨 patching vite.config.ts ...\n"
     patch_viteconfig
-    echo "\n🔨 patching tsconfig.json...\n"
+    echo "\n🔨 patching tsconfig.json ...\n"
     patch_tsconfig
     echo "\n🔨 installing modules...\n"
-    pnpm install -D tailwindcss postcss autoprefixer
-    pnpm install tailwindcss tailwindcss-animate postcss autoprefixer @tailwindcss/vite
+    pnpm add -D tailwindcss postcss autoprefixer
+    pnpm add tailwindcss tailwindcss-animate postcss autoprefixer @tailwindcss/vite
     echo "\n🔨 installing jest...\n"
-    pnpm i -D jest jest-environment-jsdom jest-transform-stub ts-jest @types/jest @testing-library/react
+    pnpm add -D jest jest-environment-jsdom jest-transform-stub ts-jest @types/jest @testing-library/react
     pnpx ts-jest config:init
     mv jest.config.js jest.config.cjs
-    echo "\n🔨 patching src/index.css...\n"
+    echo "\n🔨 patching src/index.css ...\n"
     patch_css
+    echo "\n🔨 patching src/App.tsx ...\n"
+    patch_use_local_storage
+    echo "\n🔨 patching src/hooks/use-local-storage.ts ...\n"
+    patch_app_tsx "$1"
+    echo "\n🔨 patching src/contexts/theme-context.tsx ...\n"
+    patch_theme_context
+    echo "\n🔨 patching src/contexts/theme-provider.tsx ...\n"
+    patch_theme_provider
+    echo "\n🔨 patching src/components/heading.tsx ...\n"
+    patch_heading
+    if [[ -d ~/workspace/uvxytdlp ]]; then
+	    echo " ⨁  long-press-button.tsx"
+	    cp ~/workspace/uvxytdlp/src/components/ocodo-ui/long-press-button.tsx src/components
+	    echo " ⨁  switch-state.tsx"
+	    cp ~/workspace/uvxytdlp/src/components/ocodo-ui/switch-state.tsx src/components
+	    echo " ⨁  select-state.tsx ..."
+	    cp ~/workspace/uvxytdlp/src/components/ocodo-ui/switch-state.tsx src/components
+    fi
     echo "\n🔨 installing shadcn...\n"
     yes | pnpx shadcn@latest init
-    pnpx shadcn@latest add sonner button dialog input card
+    pnpx shadcn@latest add sonner button dialog input card switch 
     
     echo "\n🔨 cleaning up...\n"
-    echo "TODO:
     # ...............
-    # Remove App.css
-    # Remove Vite.svg
-    # Replace App.tsx / index.css / index.html
     # Add SwitchState, SelectState... others
-    # ..............."
+    # ...............
     
     git init
     git remote add origin git@gitcodo.hub:ocodo/$1.git
